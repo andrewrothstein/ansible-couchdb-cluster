07bd4f670aaf737cf886e7d78226299bcda45c9c
Simon Kelly <skelly@dimagi.com>
2018-06-07 13:45:23 +0200

    make passlib optional

--- filter_plugins/couchdb_password_hash.py
+++ filter_plugins/couchdb_password_hash.py
@@ -2,18 +2,30 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import binascii
-from passlib.hash import pbkdf2_sha1
-from passlib.utils import binary
+import hashlib
+
+try:
+    from passlib.hash import pbkdf2_sha1
+    from passlib.utils import binary
+    HAS_PASSLIB = True
+except ImportError:
+    HAS_PASSLIB = False
 
 
 def couchdb_password_hash(password, salt, iterations=10):
-    dk = pbkdf2_sha1.using(rounds=iterations, salt=salt).hash(password)
-    decoded = binary.ab64_decode(dk.split('$')[-1])
-    return '-pbkdf2-{hash},{salt},{iterations}'.format(
-        hash=binascii.hexlify(decoded),
-        salt=salt,
-        iterations=iterations
-    )
+    if not HAS_PASSLIB:
+        return '-hashed-{hash},{salt}'.format(
+            hash=hashlib.sha1(password + salt),
+            salt=salt
+        )
+    else:
+        dk = pbkdf2_sha1.using(rounds=iterations, salt=salt).hash(password)
+        decoded = binary.ab64_decode(dk.split('$')[-1])
+        return '-pbkdf2-{hash},{salt},{iterations}'.format(
+            hash=binascii.hexlify(decoded),
+            salt=salt,
+            iterations=iterations
+        )
 
 
 class FilterModule(object):
